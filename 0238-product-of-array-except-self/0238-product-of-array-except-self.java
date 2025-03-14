9dd2bf53ed382642dcb4d97b68b3410ce4be2b5b
Richards <96327922+Richards-Richie@users.noreply.github.com>
2025-03-14 12:11:52 +0530

    Time: 2 ms (87.86%), Space: 55.6 MB (42.51%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,30 +1,18 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        HashMap<Integer,Integer> m=new HashMap<>();
-        int totalproduct=1;
-        for(int i=0;i<nums.length;i++){
-            m.put(nums[i],m.getOrDefault(nums[i],0)+1);
-            if(nums[i]!=0){
-                totalproduct*=nums[i];
-            }
+        int []pre=new int[nums.length];
+        pre[0]=1;
+        int []suf=new int[nums.length];
+        int n=nums.length;
+        suf[n-1]=1;
+        for(int i=1;i<n;i++){
+            pre[i]=pre[i-1]*nums[i-1];
         }
-        if(m.getOrDefault(0,0) > 1){
-            Arrays.fill(nums,0);
-            return nums;
-        }else{
-            if(m.getOrDefault(0,0) == 1){
-                for(int i=0;i<nums.length;i++){
-                    if(nums[i]!=0){
-                        nums[i]=0;
-                    }else{
-                        nums[i]=totalproduct;
-                    }
-                }
-            }else{
-                for(int i=0;i<nums.length;i++){
-                    nums[i]=totalproduct/nums[i];
-                }
-            }
+        for(int i=n-2;i>=0;i--){
+            suf[i]=suf[i+1]*nums[i+1];
+        }
+        for(int i=0;i<n;i++){
+            nums[i]=pre[i]*suf[i];
         }
         return nums;
     }
