4873b813aacc95c587a464cd7ac35f53ada87ea8
Richards <96327922+Richards-Richie@users.noreply.github.com>
2025-03-18 07:50:17 +0530

    Time: 2 ms (99.19%), Space: 48.3 MB (41.29%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,17 +1,40 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        int ele1=Integer.MIN_VALUE;
+        int ele2=Integer.MIN_VALUE;
+        int count1=0;
+        int count2=0;
         int n=nums.length;
-        List<Integer> ans=new ArrayList<>();
-        LinkedHashMap<Integer,Integer> m=new LinkedHashMap<>();
         for(int i=0;i<n;i++){
-            m.put(nums[i],m.getOrDefault(nums[i],0)+1);
-        }
-        for(Map.Entry<Integer,Integer> ele : m.entrySet()){
-            if(ele.getValue() > Math.floor(n/3)){
-                ans.add(ele.getKey());
+            if(count1==0 & ele2!=nums[i]){
+                count1=1;
+                ele1=nums[i];
+            }else if(count2 == 0 && ele1!=nums[i]){
+                count2=1;
+                ele2=nums[i];
+            }else if(ele1==nums[i]){
+                count1++;
+            }else if(ele2 == nums[i]){
+                count2++;
+            }else{
+                count1--;
+                count2--;
             }
         }
-        
+        count1=0;
+        count2=0;
+        List<Integer> ans=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(ele1 == nums[i])count1++;
+            if(ele2 == nums[i])count2++;
+        }
+        if(count1 > (n/3)){
+            ans.add(ele1);
+            
+        }
+        if(count2 > (n/3)){
+            ans.add(ele2);
+        }
         return ans;
     }
 }
