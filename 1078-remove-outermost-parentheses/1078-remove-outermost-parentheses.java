62ff903dd9d9901d671a00b1248d68fec658554d
Richards <96327922+Richards-Richie@users.noreply.github.com>
2025-04-01 13:01:45 +0530

    Time: 14 ms (12.42%), Space: 45.3 MB (14.67%) - LeetHub

---
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        int depth=0;
+        String ans="";
+        for(char c : s.toCharArray()){
+            if(c == '('){
+                if(depth > 0){
+                    ans=ans+c;
+                }
+                depth++;
+            }else{
+                depth--;
+                if(depth > 0){
+                    ans=ans+c;
+                }
+            }
+        }
+        return ans;
+    }
+}
