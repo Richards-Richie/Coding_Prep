0029811ef96e95c7e033f336a7fa66d6d053433f
Richards <96327922+Richards-Richie@users.noreply.github.com>
2025-04-12 08:10:10 +0530

    Time: 2 ms (18.63%), Space: 45.5 MB (83.42%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,36 +1,23 @@
 class Solution {
     public void setZeroes(int[][] nums) {
-        ArrayList<Integer> row=new ArrayList<>();
-        ArrayList<Integer> col=new ArrayList<>();
-        int rindex=0;
-        int cindex=0;
+        Set<Integer> r=new HashSet<>();
+        Set<Integer> c=new HashSet<>();
         int m=nums.length;
         int n=nums[0].length;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(nums[i][j] == 0){
-                    row.add(i);
-                    col.add(j);
+                    r.add(i);
+                    c.add(j);
                 }
             }
         }
-        while(rindex < row.size() ){
-            int zerorow=row.get(rindex);
-            rindex++;
-            int zerocol=col.get(cindex);
-            cindex++;
-            nums=zeros(nums,zerorow,zerocol);
-        }
-
-    }
-    public static int[][] zeros(int [][]nums,int row,int col){
-        for(int i=0;i<nums.length;i++){
-            for(int j=0;j<nums[0].length;j++){
-                if(i == row || j==col){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(r.contains(i) || c.contains(j)){
                     nums[i][j]=0;
                 }
             }
         }
-        return nums;
     }
 }
