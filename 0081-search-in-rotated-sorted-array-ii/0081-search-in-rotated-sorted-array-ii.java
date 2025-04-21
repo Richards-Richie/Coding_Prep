1312c53f910640f7d4abfa03921041a17dc16e52
Richards <96327922+Richards-Richie@users.noreply.github.com>
2025-04-21 19:50:05 +0530

    Time: 0 ms (100%), Space: 43.1 MB (91.58%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,38 +1,32 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int p=pivot(nums);
-        int n=nums.length-1;
-        if( p!=-1 ){
-            if( target >= nums[p] && target <= nums[n] ){
-                return Bsearch(nums,p,n,target);
-            }else{
-                return Bsearch(nums,0,p-1,target);
-            }
-        }else{
-            return Bsearch(nums,0,n,target);
-        }
-        
-    }
-    public boolean Bsearch(int []nums,int start,int end,int target){
+        int n=nums.length;
+        int start =0;
+        int end=n-1;
         while(start <= end){
-            int mid=start+(end-start)/2;
-            if(nums[mid] == target){
+            int mid=start + (end-start)/2;
+            if(nums[mid] == target ){
                 return true;
-            }else if(nums[mid] < target){
-                start=mid+1;
-            }else{
-                end=mid-1;
             }
+            if(nums[start] == nums[mid] && nums[mid] == nums[end]){
+                start++;
+                end--;
+            }
+            else if(nums[start] <= nums[mid]){
+                if(nums[start] <= target && target < nums[mid]){
+                    end=mid-1;
+                }else{
+                    start = mid+1;
+                }
+            }else{
+                if(nums[mid] < target && target <=nums[end]){
+                    start=mid+1;
+                }else{
+                    end=mid-1;
+                }
+            }
+
         }
         return false;
-    }
-    public int pivot(int []nums){
-        int n=nums.length;
-        for(int i=0; i<n-1; i++){
-            if(nums[i] > nums[i+1]){
-                return i+1;
-            }
-        }
-        return -1;
     }
 }
