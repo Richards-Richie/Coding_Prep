0cd19c125a42c570b6ce5ae4e83050cb0048206c
Richards <96327922+Richards-Richie@users.noreply.github.com>
2025-04-21 19:37:08 +0530

    Time: 0 ms (100%), Space: 41.9 MB (83%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,34 +1,25 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int p=pivot(nums);
-        if(p!=-1){
-            if(target >= nums[0]){
-                return bSearch(nums,target,0,p-1);
-            }else{
-                return bSearch(nums,target,p,nums.length-1);
-            }
-        }
-        return bSearch(nums,target,0,nums.length-1);
-    }
-    public int pivot(int []nums){
         int n=nums.length;
-        for(int i=0;i<n-1;i++){
-            if(nums[i] > nums[i+1]){
-                return i+1;
-            }
-        }
-        return -1;
-    }
-    public int bSearch(int[] nums,int t,int start,int end){
-        while(start <= end){
-            int mid=start+(end-start)/2;
-            if(nums[mid] == t){
+        int left=0;
+        int right=n-1;
+        while(left <= right){
+            int mid=left+(right -left)/2;
+            if(nums[mid] == target){
                 return mid;
             }
-            else if(nums[mid] > t){
-                end=mid-1;
+            if(nums[left] <= nums[mid]){
+                if(nums[left] <= target && target < nums[mid]){
+                    right=mid-1;
+                }else{
+                    left=mid+1;
+                }
             }else{
-                start=mid+1;
+                if(nums[mid] <= target && target <= nums[right]){
+                    left=mid+1;
+                }else{
+                    right=mid-1;
+                }
             }
         }
         return -1;
