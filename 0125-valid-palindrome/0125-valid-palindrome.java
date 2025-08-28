75f30eb5df73d5dd7fb47a74fea7e6540ae36480
Richards <96327922+Richards-Richie@users.noreply.github.com>
2025-08-28 15:41:31 +0530

    Time: 7 ms (45.71%), Space: 57.2 MB (5.66%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,20 +1,28 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String sbs=getString(s);
-        for(int i=0;i*2<sbs.length();i++){
-            if(sbs.charAt(i)!= sbs.charAt(sbs.length()-i-1)){
-                return false;
-            }
-        }
-        return true;
+        return checkPali(s,0,s.length()-1);
     }
-    public String getString(String s){
-        StringBuffer sb=new StringBuffer();
-        for(char c : s.toCharArray()){
-            if(Character.isLetterOrDigit(c)){
-                sb.append(Character.toLowerCase(c));
-            }
+    public boolean checkPali(String s,int left,int right){
+        if(left >= right){
+            return true;
         }
-        return sb.toString();
+
+        char le=s.charAt(left);
+        char ri=s.charAt(right);
+
+        if(!Character.isLetterOrDigit(le)){
+            return checkPali(s,left+1,right);
+        }
+
+        if(!Character.isLetterOrDigit(ri)){
+            return checkPali(s,left,right-1);
+        }
+
+        if(Character.toLowerCase(le)!= Character.toLowerCase(ri)){
+            return false;
+        }
+
+        return checkPali(s,left+1,right-1);
+
     }
 }
