5764c15270597cd27615e9540bfe5d408a3cd8f8
Richards <96327922+Richards-Richie@users.noreply.github.com>
2025-08-30 07:59:55 +0530

    Time: 0 ms (100%), Space: 41.6 MB (53.56%) - LeetHub

--- 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.java
+++ 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.java
@@ -1,20 +1,20 @@
 class Solution {
     public boolean checkString(String s) {
-        int bindex=-1;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)-'b' == 0){
-                bindex = i;
-                break;
-            }
+        int bindex =-1;
+        int n=s.length();
+        if(s.length() == 0){
+            return true;
         }
+        bindex = s.indexOf('b');
         if(bindex == -1){
             return true;
         }
-        for(int i=bindex+1;i<s.length();i++){
-            if(s.charAt(i)-'a' == 0){
+        for(int i=bindex+1;i<n;i++){
+            if(s.charAt(i) == 'a'){
                 return false;
             }
         }
         return true;
+
     }
 }
