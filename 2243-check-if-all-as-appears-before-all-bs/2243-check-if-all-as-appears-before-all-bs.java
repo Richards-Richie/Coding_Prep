c86410785d85111da12a1fbd94df9a491fbb1628
Richards <96327922+Richards-Richie@users.noreply.github.com>
2025-08-30 08:02:58 +0530

    Time: 0 ms (100%), Space: 41.5 MB (88.08%) - LeetHub

--- 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.java
+++ 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.java
@@ -1,20 +1,5 @@
 class Solution {
     public boolean checkString(String s) {
-        int bindex =-1;
-        int n=s.length();
-        if(s.length() == 0){
-            return true;
-        }
-        bindex = s.indexOf('b');
-        if(bindex == -1){
-            return true;
-        }
-        for(int i=bindex+1;i<n;i++){
-            if(s.charAt(i) == 'a'){
-                return false;
-            }
-        }
-        return true;
-
+        return  !s.contains("ba");
     }
 }
